1c91897e81298bc2e0ef82e7686700f75b52eec4
Michela Bicocchi <mbicocchi1@gmail.com>
2023-04-16 12:56:21 +0100

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Sidebar from './Sidebar';
+import Homepage from './Homepage';
+import PageNotFound from './PageNotFound'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <Routes>
+        { }
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+export default function Sidebar() {
+  return (
+    <nav>
+      <Link to='/'>Homepage</Link>
+    </nav>
+  )
+}
