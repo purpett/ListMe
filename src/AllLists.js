76e349000eaa80209a4fcf9f0322c66540f86013
Michela Bicocchi <mbicocchi1@gmail.com>
2023-04-18 12:29:44 +0100

    sidebar list links css

--- src/AllLists.js
+++ src/AllLists.js
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom"; //https://reactrouter.com/en/main/components/nav-link
-
+import "./AllLists.css"
 
 export default function AllLists(props) {
   const listLinks = props.lists.map((item, index) => {
-    return <NavLink to={`/lists/${index}`} item={item} key={index}>{item.name}</NavLink>;
+    return (
+      <li className="single-link">
+        <NavLink className="list-link" to={`/lists/${index}`} item={item} key={index}>{item.name}</NavLink>
+      </li>
+    )
   });
   return (
     <ul className="List">
